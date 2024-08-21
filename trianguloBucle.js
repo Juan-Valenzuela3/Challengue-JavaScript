61ecb51a01c43b9608ab1b8a516ba87f56c78031
Juan Valenzuela <juan_svalenzuela@soy.sena.edu.co>
2024-08-20 19:55:35 -0500

    Challengue-JavaScript

---
+++ trianguloBucle.js
@@ -0,0 +1,8 @@
+// Escribe un bucle que realice siete llamadas a console.log para mostrar el siguiente triángulo
+
+let triangulo = ''
+for (let i = 1; triangulo.length < 7; i++) {
+    triangulo += '#'
+    console.log(triangulo);
+}
+
